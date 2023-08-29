51ed7db673310ba06d2fb49722fc92664f457504
Ninda Ack <ninda2752@gmail.com>
2023-08-30 00:37:06 +0700

    Finishing App Menu

--- Ch2Git/src/main/kotlin/Main.kt
+++ Ch2Git/src/main/kotlin/Main.kt
@@ -1,7 +1,83 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class Recipe(
+    val recipeName: String,
+    val portionInPeople: Int,
+    val ingredients: List<Ingredient>,
+    val desc: String
+)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+data class Ingredient(
+    val name: String,
+    val total: Int,
+    val unit: IngredientUnit
+)
+
+enum class IngredientUnit {
+    tsp, tbs, ml, gram, pcs
+}
+
+fun Recipe.ingredientToString(): String {
+    return this.ingredients.joinToString("\n") { "${it.name} : ${it.total} ${it.unit}" }
+}
+
+
+class App() {
+    private var recipeList: MutableList<Recipe>? = null
+
+    fun initRecipe() {
+        recipeList = mutableListOf()
+        recipeList?.apply {
+            add(
+                Recipe(
+                    recipeName = "Nasi Goreng",
+                    portionInPeople = 1,
+                    ingredients = listOf(
+                        Ingredient("Nasi", total = 500, unit = IngredientUnit.gram),
+                        Ingredient("Sambal", total = 2, unit = IngredientUnit.tbs),
+                        Ingredient("Egg", total = 2, unit = IngredientUnit.pcs),
+                    ),
+                    desc = "Just Stir fry nasi with sambal and eggs"
+                )
+            )
+            add(
+                Recipe(
+                    recipeName = "Bakso",
+                    portionInPeople = 1,
+                    ingredients = listOf(
+                        Ingredient("Bakso", total = 20, unit = IngredientUnit.pcs),
+                        Ingredient("Daging", total = 100, unit = IngredientUnit.gram),
+                        Ingredient("Broth", total = 200, unit = IngredientUnit.ml),
+                    ),
+                    desc = "Just Stir fry nasi with sambal and eggs"
+                )
+            )
+        }
+    }
+    fun printRecipe() {
+        if (recipeList == null) {
+            println("Recipe is Empty")
+            return
+        }
+        recipeList?.forEachIndexed { index, recipe ->
+            println(
+                """
+                =============================
+                Recipe number : ${index + 1}
+                =============================
+                How to make ${recipe.recipeName}
+                -----------------------------
+                Portion : ${recipe.portionInPeople} Per People
+            """.trimIndent()
+            )
+            println("Ingredients :\n${recipe.ingredientToString()} \n")
+            println("Step to make :\n${recipe.desc}")
+            println()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val app = App()
+    app.initRecipe()
+    app.printRecipe()
 }
